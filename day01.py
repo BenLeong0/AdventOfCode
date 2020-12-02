cbc1485a96904690b780ff992bfae72f6838ed64
Ben <benleong0@gmail.com>
2020-12-02 17:40:02 +0000

    day 1 and 2

---
+++ day01.py
@@ -0,0 +1,21 @@
+expenses = [];
+
+with open('day01input', 'r') as f:
+    for line in f:
+        expenses.append(int(line[:-1]))
+
+def f(array):
+    for i in range(len(array)):
+        for j in range(i+1, len(array)):
+            if array[i] + array[j] == 2020:
+                return array[i] * array[j]
+
+def g(array):
+    for i in range(len(array)):
+        for j in range(i+1, len(array)):
+            for k in range(j+1, len(array)):
+                if array[i] + array[j] + array[k] == 2020:
+                    return array[i] * array[j] * array[k]
+
+print(f(expenses))
+print(g(expenses))
---
+++ day02.py
@@ -0,0 +1,32 @@
+inputs = [];
+
+ranges = [];
+letters = [];
+passwords = [];
+
+
+with open('day02input', 'r') as f:
+    for line in f:
+        inputs.append(line[:-1].split(' '));
+
+for input in inputs:
+    ranges.append(input[0].split('-'));
+    letters.append(input[1][0]);
+    passwords.append(input[2])
+
+
+# counter = 0
+# for i in range(len(passwords)):
+#     letter_count = passwords[i].count(letters[i]);
+#     if int(ranges[i][0]) <= letter_count <= int(ranges[i][1]):
+#         counter += 1;
+
+counter = 0
+for i in range(len(passwords)):
+    a = (letters[i] == passwords[i][int(ranges[i][0]) - 1])
+    b = (letters[i] == passwords[i][int(ranges[i][1]) - 1])
+    if (a and not b) or (b and not a):
+        counter += 1;
+
+
+print(counter)
