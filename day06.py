a3e88549f37b9fd3d1cb60cdb68b18fb595af520
Ben <benleong0@gmail.com>
2020-12-07 13:17:41 +0000

    day6 cleaner

---
+++ day06.py
@@ -0,0 +1,38 @@
+from string import ascii_lowercase
+
+count = 0
+
+
+## PART 1
+# with open('day06input', 'r') as f:
+#     group = set()
+#     while True:
+#         line = f.readline()
+#         if not line:
+#             count += len(group)
+#             group = set()
+#             break
+#         line = line[:-1]
+#         if not line:
+#             count += len(group)
+#             group = set()
+#         else:
+#             group = group.union({i for i in line})
+
+
+with open('day06input', 'r') as f:
+    group = {i for i in ascii_lowercase}
+    while True:
+        line = f.readline()
+        if not line:
+            count += len(group)
+            break
+        line = line[:-1]
+        if not line:
+            count += len(group)
+            group = {i for i in ascii_lowercase}
+        else:
+            group = {i for i in group if i in line}
+
+
+print(count)
