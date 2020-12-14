4fc86f43ec7825bd01ae5502d37ed6076dc70019
Ben <benleong0@gmail.com>
2020-12-14 12:26:41 +0000

    day13

---
+++ day13.py
@@ -0,0 +1,35 @@
+from math import inf
+
+instructions = []
+
+with open('inputs/day13input', 'r') as f:
+    startTime = int(f.readline())
+    timeList = f.readline()
+    if timeList[-1] == '\n':
+        timeList = timeList[:-1]
+    times = [int(x) if x != 'x' else x for x in timeList.split(',')]
+
+
+def partOne():
+    min = (0, inf)
+    for id in (x for x in times if x != 'x'):
+        if id - startTime%id < min[1]:
+            min = (id, id - startTime%id)
+    print(min, '=>', min[0]*min[1])
+    return
+
+
+def partTwo():
+    newStart = 0
+    inc = 1
+    for (i, id) in enumerate(times):
+        if id != 'x':
+            while (id - newStart%id)%id != i%id:
+                newStart += inc
+            inc *= id
+    print(newStart)
+    return
+
+
+partOne()
+partTwo()
