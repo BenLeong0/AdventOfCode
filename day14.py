bda404bce6e918715d8e8d2079069f3dbde39264
Ben <benleong0@gmail.com>
2020-12-14 12:47:36 +0000

    day14notes

--- day14.py
+++ day14.py
@@ -4,11 +4,15 @@ def partOne():
         line = f.readline()[:-1]
         while line:
             if line.split(' ')[0] == 'mask':
+                # Save mask as dict, with pos:bit
                 mask = {key:value for (key, value) in enumerate(line.split(' ')[2]) if value!='X'}
             else:
+                # Convert to binary
                 b = format(int(line.split(' ')[2]), '036b')
+                # Apply each element of the mask
                 for (key,value) in mask.items():
                     b = b[:key] + value + b[key+1:]
+                # Convert back to decimal and save to mem(ory)
                 mem[line.split(' ')[0][4:-1]] = int(b,2)
             line = f.readline()[:-1]
     return print('Part One:  ', sum(mem.values()))
@@ -20,15 +24,20 @@ def partTwo():
         line = f.readline()[:-1]
         while line:
             if line.split(' ')[0] == 'mask':
+                # Save mask as dict, with pos:bit
                 mask = {key:value for (key, value) in enumerate(line.split(' ')[2])}
             else:
+                # Convert address to binary and place in array
                 a = [format(int(line.split(' ')[0][4:-1]), '036b')]
                 for (key,value) in mask.items():
                     if value == '1':
+                        # Update each elemnt of array
                         a = [x[:key] + '1' + x[key+1:] for x in a]
                     elif value == 'X':
+                        # If value=X, clone all elements of array with pos = 0 and 1
                         a = [x[:key] + '1' + x[key+1:] for x in a] + [x[:key] + '0' + x[key+1:] for x in a]
                 for address in a:
+                    # Convert all addresses back to decimal and save values
                     mem[int(address,2)] = int(line.split(' ')[2])
             line = f.readline()[:-1]
     return print('Part Two:  ', sum(mem.values()))
