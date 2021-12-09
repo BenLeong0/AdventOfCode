1d5d895a6a8cfd9a0b771b5cd0180c2d459eca70
benleong0 <benleong@myurbanjungle.com>
2021-12-09 09:05:09 +0000

    fix range

--- 2021/day1.py
+++ 2021/day1.py
@@ -17,7 +17,7 @@ print(count_increases(full_input))
 
 # Part 2
 def sliding_window_increases(depths: List[int]) -> int:
-    return sum([sum(depths[i:i+3])<sum(depths[i+1:i+4]) for i in range(len(depths)-2)])
+    return sum([sum(depths[i:i+3])<sum(depths[i+1:i+4]) for i in range(len(depths)-3)])
 
 assert sliding_window_increases(test_input) == 5
 print(sliding_window_increases(full_input))
