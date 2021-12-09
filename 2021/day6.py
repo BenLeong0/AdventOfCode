061983a88f689300fc1fd59fb5155c867e4f0a9d
benleong0 <benleong@myurbanjungle.com>
2021-12-09 08:45:43 +0000

    remove bad fn

--- 2021/day6.py
+++ 2021/day6.py
@@ -8,22 +8,6 @@ with open("day6.in", "r", newline="\n") as readfile:
     full_input = [int(x) for x in readfile.readline()[:-1].split(',')]
 
 
-# Part 1
-def find_number_of_lanternfish_old(lantern_fish_timers: List[int], num_days: int=80) -> int:
-    for _ in range(num_days):
-        new_fish = 0
-        for (i, timer) in enumerate(lantern_fish_timers):
-            if timer == 0:
-                lantern_fish_timers[i] = 6
-                new_fish += 1
-            else:
-                lantern_fish_timers[i] -= 1
-
-        lantern_fish_timers += [8 for __ in range(new_fish)]
-
-    return len(lantern_fish_timers)
-
-
 def find_number_of_lanternfish(lantern_fish_timers: List[int], num_days: int=80) -> int:
     num_of_fish = [lantern_fish_timers.count(i) for i in range(9)]
     for _ in range(num_days):
@@ -42,6 +26,7 @@ def find_number_of_lanternfish(lantern_fish_timers: List[int], num_days: int=80)
     return sum(num_of_fish)
 
 
+# Part 1
 assert find_number_of_lanternfish(copy.deepcopy(test_input), 18) == 26
 assert find_number_of_lanternfish(copy.deepcopy(test_input)) == 5934
 print(find_number_of_lanternfish(copy.deepcopy(full_input)))
