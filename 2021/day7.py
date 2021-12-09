896423d8d5638f4b6c4bf5e7a0b8a41478453de7
benleong0 <benleong@myurbanjungle.com>
2021-12-09 08:43:54 +0000

    fns

--- 2021/day7.py
+++ 2021/day7.py
@@ -22,13 +22,14 @@ print(find_min_fuel(full_input))
 
 # Part 2
 def find_min_fuel_exponential(starting_positions: List[int]) -> int:
-    triangle_number = lambda n: (n*(n+1))//2
     sorted_positions = sorted(starting_positions)
-    curr_min = float("inf")
-    for pos in range(sorted_positions[-1] + 1):
-        fuel_usage = sum([triangle_number(abs(x-pos)) for x in sorted_positions])
-        curr_min = min(curr_min, fuel_usage)
-    return curr_min
+
+    def get_triangle_number(n: int) -> int:
+        return ((n) * (n + 1)) // 2
+    def get_total_fuel_usage(pos: int) -> int:
+        return sum([get_triangle_number(abs(x-pos)) for x in sorted_positions])
+
+    return min([get_total_fuel_usage(pos) for pos in range(sorted_positions[-1] + 1)])
 
 assert find_min_fuel_exponential(test_input) == 168
 print(find_min_fuel_exponential(full_input))
