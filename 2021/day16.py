c59caa0720afc55ae61f47268f3291fc3ddf96f4
benleong0 <benleong0@gmail.com>
2021-12-16 22:57:09 +0000

    day16

--- 2021/day16.py
+++ 2021/day16.py
@@ -1,48 +1,127 @@
-from typing import List, Tuple
+from typing import Tuple
 
 
 test_input: str = "D2FE28"
 
 with open("day16.in", "r", newline="\n") as readfile:
-    full_input = readfile.readline()[:-1]
+    full_input = readfile.read().splitlines()[0]
 
-# V = 110 = 6 = version
-# type = 100 = 4 => literal
-# next 5 = 1 0111 (start with 1 => continue) => 0111
-# next 5 = 1 1110 (start with 1 => continue) => 01111110
-# next 5 = 0 0101 (start with 0 => final group) => 011111100101 = 2021
-# last 3 = 000 = all 0 => ignore
-# => value =
 
 # Part 1
-def get_packet_info(hex_input: str) -> Tuple[int,int]:
-    """Return tuple of (version sum, packet length)"""
-    binary_rep = bin(int(hex_input, 16))[2:].zfill(4*len(hex_input)).rstrip("0")
-    version = int(binary_rep[0:3], 2)
-    type_id = int(binary_rep[3:6], 2)
+def get_version_sum(hex_input: str) -> int:
+    binary_rep = bin(int(hex_input, 16))[2:].zfill(4*len(hex_input))
 
-    if type_id == 4:
-        curr_num = ''
-        i = 6
-        while True:
-            curr_packet = binary_rep[i:i+5]
-            curr_num += curr_packet[1:]
-            if curr_packet[0] == '0':
-                break
-            i += 5
-        return version, i
+    def get_packet_info(start_idx: int = 0) -> Tuple[int,int]:
+        """Return tuple of form `(version sum, packet length)`"""
+        if "1" not in binary_rep[start_idx:]:
+            return 0, len(binary_rep)
 
-    else:
-        length_type_id = binary_rep[6]
+        version = int(binary_rep[start_idx+0:start_idx+3], 2)
+        type_id = int(binary_rep[start_idx+3:start_idx+6], 2)
+
+        if type_id == 4:
+            curr_num = ''
+            curr_idx = start_idx + 6
+            while '1' in binary_rep[curr_idx:]:
+                curr_packet = binary_rep[curr_idx:curr_idx+5]
+                curr_num += curr_packet[1:]
+                curr_idx += 5
+                if curr_packet[0] == '0':
+                    break
+            return version, curr_idx
+
+        length_type_id = binary_rep[start_idx+6]
         if length_type_id == '0':
-            length = int(binary_rep[7:22])
-            i = 6
-            while i < length+6:
-                new_version, new_length = get_packet_info(hex_input[i+6:])
+            length = int(binary_rep[start_idx+7:start_idx+22], 2)
+            curr_idx = start_idx + 22
+            while (curr_idx < start_idx + length + 22) and ('1' in binary_rep[curr_idx:]):
+                new_version, curr_idx = get_packet_info(curr_idx)
                 version += new_version
-                i += new_length
-            return version, i
-        else:
-            num_of_subpackets = int(binary_rep[7:18])
+            return version, curr_idx
 
-print(get_packet_info("D2FE28"))
+        else:
+            num_of_subpackets = int(binary_rep[start_idx+7:start_idx+18], 2)
+            curr_idx = start_idx + 18
+            for _ in range(num_of_subpackets):
+                new_version, curr_idx = get_packet_info(curr_idx)
+                version += new_version
+            return version, curr_idx
+
+    return get_packet_info()[0]
+
+assert get_version_sum("D2FE28") == 6
+assert get_version_sum("8A004A801A8002F478") == 16
+assert get_version_sum("620080001611562C8802118E34") == 12
+assert get_version_sum("C0015000016115A2E0802F182340") == 23
+assert get_version_sum("A0016C880162017C3686B18A3D4780") == 31
+print(get_version_sum(full_input))
+
+
+# Part 2
+def evaluate_with_operations(hex_input: str) -> int:
+    binary_rep = bin(int(hex_input, 16))[2:].zfill(4*len(hex_input))
+
+    operations = {
+        0: lambda x,y: x+y if x is not None else y,
+        1: lambda x,y: x*y if x is not None else y,
+        2: lambda x,y: min(x,y) if x is not None else y,
+        3: lambda x,y: max(x,y) if x is not None else y,
+        5: lambda x,y: (1 if y < x else 0) if x is not None else y,
+        6: lambda x,y: (1 if y > x else 0) if x is not None else y,
+        7: lambda x,y: (1 if y == x else 0) if x is not None else y,
+    }
+
+    def get_packet_info(start_idx: int = 0) -> Tuple[int,int,int]:
+        """Return tuple of form `(version sum, packet length, value)`"""
+        if "1" not in binary_rep[start_idx:]:
+            return 0, len(binary_rep)
+
+        version = int(binary_rep[start_idx+0:start_idx+3], 2)
+        type_id = int(binary_rep[start_idx+3:start_idx+6], 2)
+
+        if type_id == 4:
+            curr_value_bin = ''
+            curr_idx = start_idx + 6
+            while '1' in binary_rep[curr_idx:]:
+                curr_packet = binary_rep[curr_idx:curr_idx+5]
+                curr_value_bin += curr_packet[1:]
+                curr_idx += 5
+                if curr_packet[0] == '0':
+                    break
+            return version, curr_idx, int(curr_value_bin, 2)
+
+        length_type_id = binary_rep[start_idx+6]
+        operation = operations[type_id]
+        curr_value = None
+
+        idx_past_header = start_idx + 7
+
+        if length_type_id == '0':
+            length = int(binary_rep[idx_past_header:idx_past_header+15], 2)
+            curr_idx = idx_past_header + 15
+            while (curr_idx < idx_past_header + length + 15) and ('1' in binary_rep[curr_idx:]):
+                new_version, curr_idx, new_value = get_packet_info(curr_idx)
+                version += new_version
+                curr_value = operation(curr_value, new_value)
+            return version, curr_idx, curr_value
+
+        if length_type_id == '1':
+            num_of_subpackets = int(binary_rep[idx_past_header:idx_past_header+11], 2)
+            curr_idx = idx_past_header + 11
+            for _ in range(num_of_subpackets):
+                new_version, curr_idx, new_value = get_packet_info(curr_idx)
+                version += new_version
+                curr_value = operation(curr_value, new_value)
+            return version, curr_idx, curr_value
+
+    return get_packet_info()[2]
+
+assert evaluate_with_operations("C200B40A82") == 3
+assert evaluate_with_operations("04005AC33890") == 54
+assert evaluate_with_operations("880086C3E88112") == 7
+assert evaluate_with_operations("CE00C43D881120") == 9
+assert evaluate_with_operations("D8005AC2A8F0") == 1
+assert evaluate_with_operations("F600BC2D8F") == 0
+assert evaluate_with_operations("9C005AC2F8F0") == 0
+assert evaluate_with_operations("9C0141080250320F1802104A08") == 1
+print(evaluate_with_operations(full_input))
