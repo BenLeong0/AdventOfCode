a977fb88f556b78200745f1344c05516e8cccefa
ben-laptop <benleong0@gmail.com>
2022-12-01 23:10:39 +0000

    generator

--- 2022/day01.py
+++ 2022/day01.py
@@ -4,7 +4,7 @@ from itertools import combinations
 INPUT_FILE = "input/day01.in"
 
 print(max(sum(map(int, x.split("\n"))) for x in open(INPUT_FILE).read().split("\n\n")))
-print(max(sum(y) for y in combinations([sum(map(int, x.split("\n"))) for x in open(INPUT_FILE).read().split("\n\n")], r=3)))
+print(max(sum(y) for y in combinations((sum(map(int, x.split("\n"))) for x in open(INPUT_FILE).read().split("\n\n")), r=3)))
 
 
 ####
