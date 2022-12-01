f5f38c18d8c66e01cff9e7fe3eecfc575ffb6d1c
ben-laptop <benleong0@gmail.com>
2022-12-01 22:57:24 +0000

    global

--- 2022/day01.py
+++ 2022/day01.py
@@ -1,4 +1,7 @@
 from itertools import combinations
 
-print(max(sum(map(int, x.split("\n"))) for x in open("input/day01.in").read().split("\n\n")))
-print(max(sum(y) for y in combinations([sum(map(int, x.split("\n"))) for x in open("input/day01.in").read().split("\n\n")], r=3)))
+
+INPUT_FILE = "input/day01.in"
+
+print(max(sum(map(int, x.split("\n"))) for x in open(INPUT_FILE).read().split("\n\n")))
+print(max(sum(y) for y in combinations([sum(map(int, x.split("\n"))) for x in open(INPUT_FILE).read().split("\n\n")], r=3)))
