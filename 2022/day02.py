1aa6b6d5e6e8359c868cd7fb4e4fbfba84ea2edd
benleong0 <benleong@myurbanjungle.com>
2022-12-02 09:28:32 +0000

    part2

--- 2022/day02.py
+++ 2022/day02.py
@@ -2,5 +2,7 @@
 INPUT_FILE = "input/day02.in"
 
 part1 = sum(((" XYZ".index(b)-"ABC".index(a))%3)*3+" XYZ".index(b) for a,_,b in open(INPUT_FILE).read().split("\n"))
+part2 = sum(("XYZ".index(b)-1+"ABC".index(a))%3+1+"XYZ".index(b)*3 for a,_,b in open(INPUT_FILE).read().split("\n"))
 
 print(part1)
+print(part2)
