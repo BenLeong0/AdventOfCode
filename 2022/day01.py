339422b48544cbd225f5065082cd225d8e91083b
benleong0 <benleong@myurbanjungle.com>
2022-12-02 09:02:48 +0000

    format

--- 2022/day01.py
+++ 2022/day01.py
@@ -3,11 +3,14 @@ from itertools import combinations
 
 INPUT_FILE = "input/day01.in"
 
-print(max(sum(map(int, x.split("\n"))) for x in open(INPUT_FILE).read().split("\n\n")))
-print(max(sum(y) for y in combinations((sum(map(int, x.split("\n"))) for x in open(INPUT_FILE).read().split("\n\n")), r=3)))
+part1 = max(sum(map(int, x.split("\n"))) for x in open(INPUT_FILE).read().split("\n\n"))
+part2 = max(sum(y) for y in combinations((sum(map(int, x.split("\n"))) for x in open(INPUT_FILE).read().split("\n\n")), r=3))
+
+print(part1)
+print(part2)
 
 
-####
+######
 
 from utils import file_to_list
 
