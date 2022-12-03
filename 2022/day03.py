4141cadeaa79f758fe8405d8985a626e68c820d8
benleong0 <benleong@myurbanjungle.com>
2022-12-03 11:24:55 +0000

    the most fucked up one yet

---
+++ 2022/day03.py
@@ -0,0 +1,11 @@
+from string import ascii_letters
+
+
+# INPUT_FILE = "input/day03test.in"
+INPUT_FILE = "input/day03.in"
+
+part1 = sum(1+ascii_letters.index([c for c in x[:len(x)//2]if c in x[len(x)//2:]][0])for x in open(INPUT_FILE).read().split("\n"))
+part2 = sum(1+ascii_letters.index(k)for k in [[[c for c in f[3*i]if c in f[3*i+1]and c in f[3*i+2]][0]for i in range(len(f)//3)]for f in [open(INPUT_FILE).read().split("\n")]][0])
+
+print(part1)
+print(part2)
