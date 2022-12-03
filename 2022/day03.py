cedc8f70a5eaa063143ec0e1d3109f320050f727
benleong0 <benleong@myurbanjungle.com>
2022-12-03 14:28:40 +0000

    delete comment

--- 2022/day03.py
+++ 2022/day03.py
@@ -6,7 +6,6 @@ INPUT_FILE = "inputs/day03.in"
 
 part1 = sum(1+ascii_letters.index([c for c in x[:len(x)//2]if c in x[len(x)//2:]][0])for x in open(INPUT_FILE).read().split("\n"))
 part2 = sum(1+ascii_letters.index(k)for k in [[[c for c in f[i]if c in f[i+1]and c in f[i+2]][0]for i in range(0,len(f),3)]for f in [open(INPUT_FILE).read().split("\n")]][0])
-# Note: the `for f in [open(INPUT_FILE).read().split("\n")]][0]` in part2 is just to alias the file as `f`
 
 print(part1)
 print(part2)
