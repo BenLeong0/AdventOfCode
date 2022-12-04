69561469874d54fc564b6da5f4aba4e2be20164c
benleong0 <benleong@myurbanjungle.com>
2022-12-04 10:57:28 +0000

    smaller with nots

--- 2022/day04.py
+++ 2022/day04.py
@@ -4,7 +4,7 @@ import re
 INPUT_FILE = "inputs/day04.in"
 
 part1 = sum(a<=c<=d<=b or c<=a<=b<=d for a,b,c,d in [map(int,re.split(r"[,-]", r)) for r in open(INPUT_FILE).read().split("\n")])
-part2 = sum(a<=d and b>=c or a>=d and b<=c for a,b,c,d in [map(int,re.split(r"[,-]", r)) for r in open(INPUT_FILE).read().split("\n")])
+part2 = sum(not(a>d or b<c)for a,b,c,d in [map(int,re.split(r"[,-]", r)) for r in open(INPUT_FILE).read().split("\n")])
 
 print(part1)
 print(part2)
