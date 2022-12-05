624cadb92a05af3be903f577a5d9f8b0d2d9a0d9
benleong0 <benleong@myurbanjungle.com>
2022-12-05 23:26:50 +0000

    no kw

--- 2022/day05.py
+++ 2022/day05.py
@@ -4,8 +4,8 @@ from re import findall
 # INPUT_FILE = "inputs/day05test.in"
 INPUT_FILE = "inputs/day05.in"
 
-part1 = "".join(s[0]for s in[reduce(lambda n,t:{**n,t[1]:n[t[1]][t[0]:],t[2]:n[t[1]][:t[0]][::-1]+n[t[2]]},[list(map(int,findall("\d+",r)))for r in f[f.index("")+1:]],[{i:"".join(s).replace(" ","")for i,s in enumerate(list(zip(*f[:f.index("")-1]))[1::4],start=1)}][0])for f in[open(INPUT_FILE).read().split("\n")]][0].values())
-part2 = "".join(s[0]for s in[reduce(lambda n,t:{**n,t[1]:n[t[1]][t[0]:],t[2]:n[t[1]][:t[0]]+n[t[2]]},[list(map(int,findall("\d+",r)))for r in f[f.index("")+1:]],[{i:"".join(s).replace(" ","")for i,s in enumerate(list(zip(*f[:f.index("")-1]))[1::4],start=1)}][0])for f in[open(INPUT_FILE).read().split("\n")]][0].values())
+part1 = "".join(s[0]for s in[reduce(lambda n,t:{**n,t[1]:n[t[1]][t[0]:],t[2]:n[t[1]][:t[0]][::-1]+n[t[2]]},[list(map(int,findall("\d+",r)))for r in f[f.index("")+1:]],[{i:"".join(s).replace(" ","")for i,s in enumerate(list(zip(*f[:f.index("")-1]))[1::4],1)}][0])for f in[open(INPUT_FILE).read().split("\n")]][0].values())
+part2 = "".join(s[0]for s in[reduce(lambda n,t:{**n,t[1]:n[t[1]][t[0]:],t[2]:n[t[1]][:t[0]]+n[t[2]]},[list(map(int,findall("\d+",r)))for r in f[f.index("")+1:]],[{i:"".join(s).replace(" ","")for i,s in enumerate(list(zip(*f[:f.index("")-1]))[1::4],1)}][0])for f in[open(INPUT_FILE).read().split("\n")]][0].values())
 
 print(part1)
 print(part2)
