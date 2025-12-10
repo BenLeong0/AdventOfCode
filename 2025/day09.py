9fc90c0f73343b5552db44cddb8e30e349fff176
benleong0 <benleong@myurbanjungle.com>
2025-12-10 09:12:07 +0000

    part 2

--- 2025/day09.py
+++ 2025/day09.py
@@ -11,3 +11,8 @@ part1ol = lambda filename: max((abs(int(a[0])-int(b[0]))+1)*(abs(int(a[1])-int(b
 
 print(part1ol(INPUT_FILE_TEST))
 print(part1ol(INPUT_FILE))
+
+part2ol = lambda filename: [pairs:=[(a,b) for a,b in combinations((list(map(int,x.split(","))) for x in open(filename).read().split("\n")[:-1]),2) if a[0]==b[0] or a[1]==b[1]],max((abs(a[0]-b[0])+1)*(abs(a[1]-b[1])+1) for a,b in combinations((list(map(int,x.split(","))) for x in open(filename).read().split("\n")[:-1]),2) if all(min(a[0],b[0])>=max(x[0],y[0]) or max(a[0],b[0])<=min(x[0],y[0]) or min(a[1],b[1])>=max(x[1],y[1]) or max(a[1],b[1])<=min(x[1],y[1]) for x,y in pairs))][1]
+
+print(part2ol(INPUT_FILE_TEST))
+print(part2ol(INPUT_FILE))
